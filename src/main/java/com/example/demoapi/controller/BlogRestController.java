d176b757e3a3d1a0af2c72f73075c230da5ed8e2
minhcong213274 <gathidungru@gmail.com>
2023-07-11 14:31:28 +0700

    cong

--- src/main/java/com/example/demoapi/controller/BlogRestController.java
+++ src/main/java/com/example/demoapi/controller/BlogRestController.java
@@ -1,13 +1,47 @@
 package com.example.demoapi.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demoapi.model.Blog;
+import com.example.demoapi.service.blog.IBlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/blog")
 @CrossOrigin("*")
 public class BlogRestController {
-
-
+    @Autowired
+    public IBlogService blogService;
+    @GetMapping("")
+    public ResponseEntity<Iterable<Blog>> listBlog(){
+        return new ResponseEntity<>(blogService.findAll(), HttpStatus.OK);
+    }
+    @GetMapping("/api")
+    public ResponseEntity<Blog> findById(@PathVariable Long id){
+        Optional<Blog> optionalBlog = blogService.findById(id);
+        if (!optionalBlog.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(optionalBlog.get(),HttpStatus.OK);
+    }
+    @PostMapping("/api")
+    public ResponseEntity<Blog> createBlog(@RequestBody Blog blog){
+        return new ResponseEntity<>(blogService.save(blog),HttpStatus.CREATED);
+    }
+    @DeleteMapping("/{id}")
+    public void remove(@PathVariable Long id){
+        blogService.deleteById(id);
+    }
+    @PutMapping("/api/{id}")
+    public ResponseEntity<Blog> update(@PathVariable Long id,@RequestBody Blog blog){
+        Optional<Blog> optionalBlog = blogService.findById(id);
+        if (!optionalBlog.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        blog.setBlogId(id);
+        return new ResponseEntity<>(blogService.save(blog),HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/demoapi/controller/UserRestController.java
+++ src/main/java/com/example/demoapi/controller/UserRestController.java
@@ -1,11 +1,49 @@
 package com.example.demoapi.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demoapi.model.Blog;
+import com.example.demoapi.model.User;
+import com.example.demoapi.service.blog.IBlogService;
+import com.example.demoapi.service.user.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
 @CrossOrigin("*")
 public class UserRestController {
+    @Autowired
+    public IUserService userService;
+    @GetMapping("")
+    public ResponseEntity<Iterable<User>> listUser(){
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+    @GetMapping("/api")
+    public ResponseEntity<User> findUserById(@PathVariable Long id){
+        Optional<User> optionalUser = userService.findById(id);
+        if (!optionalUser.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(optionalUser.get(),HttpStatus.OK);
+    }
+    @PostMapping("/api")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        return new ResponseEntity<>(userService.save(user),HttpStatus.CREATED);
+    }
+    @DeleteMapping("/{id}")
+    public void removeUser(@PathVariable Long id){
+        userService.deleteById(id);
+    }
+    @PutMapping("/api/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id,@RequestBody User user){
+        Optional<User> optionalUser = userService.findById(id);
+        if (!optionalUser.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        user.setUserId(id);
+        return new ResponseEntity<>(userService.save(user),HttpStatus.OK);
+    }
 }
